53bc520615a8388fd36af464442fd3740e00aaeb
cybe44oot <lswagy22@gmail.com>
2026-05-07 17:49:56 +0300

    adding changes

--- modules/network/load_balancers.tf
+++ modules/network/load_balancers.tf
@@ -8,12 +8,14 @@ security_groups = [aws_security_group.public_lb.id]
 subnets = [aws_subnet.public_lb.id]
 tags = { Name = "malaa-public-alb" }
 }
+
 resource "aws_lb_target_group" "public" {
 name = "tg-public"
 port = 80
 protocol = "HTTP"
 vpc_id = aws_vpc.main.id
 }
+
 resource "aws_lb_listener" "public_http" {
   load_balancer_arn = aws_lb.public.arn
   port              = 80
@@ -35,6 +37,7 @@ security_groups = [aws_security_group.servers.id]
 subnets = [aws_subnet.private_servers.id]
 tags = { Name = "malaa-private-servers-alb" }
 }
+
 resource "aws_lb_target_group" "servers" {
 name = "tg-servers"
 port = 80
@@ -51,6 +54,7 @@ load_balancer_type = "network"
 subnets = [aws_subnet.private_database.id]
 tags = { Name = "malaa-private-db-nlb" }
 }
+
 resource "aws_lb_target_group" "db" {
 name = "tg-db"
 port = 26257
